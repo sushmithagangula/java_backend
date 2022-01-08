64d33520bd265e4190f016dead4b3345357cbc36
sushmitha.gangula <sushmitha.gangula@collabera.com>
2022-01-08 20:32:03 +0530

    cls:car

---
+++ java-simple-app/week1/days2/src/main/java/clsobj/Car.java
@@ -0,0 +1,21 @@
+package clsobj;
+
+public class Car {
+    // state
+    public int speed;
+
+    // constructor used to initialize the state at the time of
+    // object creation
+    //default
+    public Car() { speed = 10;  }
+
+    //parameterized
+    public Car(int sp) {
+        speed = sp;
+    }
+
+    public void speedUp() {
+        speed += 10;
+    }
+
+}

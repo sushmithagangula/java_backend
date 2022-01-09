1ebe90df0a9ac0a2cff2265e60b9d8ba637be674
sushmitha.gangula <sushmitha.gangula@collabera.com>
2022-01-09 15:57:54 +0530

    ConnectivityTests

--- java-simple-app/week1/day3/java-jdbc-connectivity/src/test/java/ConnectivityTests.java
+++ java-simple-app/week1/day3/java-jdbc-connectivity/src/test/java/ConnectivityTests.java
@@ -1,15 +1,15 @@
-import jb.Connectivity;
+import db.Connectivity;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class ConnectivityTests {
-
     @DisplayName("Testing database connection")
     @Test
+
     void testConnectivity() {
-        Assertion.assertDoesNotThrow( () -> {
-            Connectivity con = new Connectivity();
+        Assertions.assertDoesNotThrow( () -> {
+            Connectivity con=new Connectivity();
             Assertions.assertNotNull(con.getConnection());
         });
     }

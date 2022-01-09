1322425c7d70c8c96f0a76c483175c2d1adcf857
sushmitha.gangula <sushmitha.gangula@collabera.com>
2022-01-09 15:56:09 +0530

    Connectivity

--- java-simple-app/week1/day3/java-jdbc-connectivity/src/main/java/db/Connectivity.java
+++ java-simple-app/week1/day3/java-jdbc-connectivity/src/main/java/db/Connectivity.java
@@ -1,11 +1,10 @@
-package jb;
+package db;
 
-        import java.sql.Connection;
-        import java.sql.DriverManager;
-        import java.sql.SQLException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
-public class Connection{
-
+public class Connectivity {
     private final Connection connection;
 
     public Connectivity() throws ClassNotFoundException, SQLException {

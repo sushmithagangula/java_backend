e43f81faa24ca42d1af261bcf85cc1320f4fcb32
sushmitha.gangula <sushmitha.gangula@collabera.com>
2022-01-09 15:13:27 +0530

    BadNumberExeception

---
+++ java-simple-app/week1/day3/src/main/java/exception/BadNumberException.java
@@ -0,0 +1,12 @@
+package exception;
+
+public class BadNumberException extends RuntimeException
+    {
+        public BadNumberException() {
+        }
+
+       public BadNumberException(String message)
+        {
+            super(message);
+        }
+}

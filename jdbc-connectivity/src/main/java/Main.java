9e171974eb253a1bca6e24b2d596922e366c86e1
sushmitha.gangula <sushmitha.gangula@collabera.com>
2022-01-10 17:03:31 +0530

    case study week1 day3:bank operation

---
+++ jdbc-connectivity/src/main/java/Main.java
@@ -0,0 +1,39 @@
+import db.Connectivity;
+import service.DbService;
+
+import java.sql.Date;
+import java.sql.SQLException;
+import java.time.LocalDate;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        try {
+            Connectivity connectivity = new Connectivity();
+
+            DbService service = new DbService(connectivity.getConnection());
+//            int cnt = service.create(
+//                    7,
+//                    "klm",
+//                    Date.valueOf(LocalDate.now()),
+//                    true
+//            );
+//            if(cnt > 0) {
+//                System.out.println("Employee Created Successfully");
+//            }
+
+//            service.find();
+
+//            service.findEmployeeByName("abc");
+
+//            service.txnDemo(60, "kol", Date.valueOf(LocalDate.now()), true, 100);
+
+            service.taxIsolationDemo(789, "mjk", Date.valueOf(LocalDate.now()), true);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}

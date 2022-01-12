2564226104acff33ca06f9fd31de7814e810007e
sushmitha.gangula <sushmitha.gangula@collabera.com>
2022-01-12 16:43:41 +0530

    case-study-week1

--- case-study-week1/src/main/java/service/DbService.java
+++ case-study-week1/src/main/java/service/DbService.java
@@ -20,28 +20,46 @@ public class DbService {
     }
 
     public void printAllAccounts() throws SQLException {
-        String sql="Select * from Bank";
+        String sql = "Select * from Bank";
         PreparedStatement ps = connection.prepareStatement(sql);
         ResultSet rs = ps.executeQuery();
-        while(rs.next()){
+        while (rs.next()) {
             int acNum = rs.getInt(1);
             double amt = rs.getDouble(2);
             String acHldNm = rs.getString(3);
             Date crDt = rs.getDate(4);
             boolean status = rs.getBoolean(5);
-            System.out.println("account number"+ acNum);
-            System.out.println("amount"+ amt);
-            System.out.println("account holder name"+acHldNm);
-            System.out.println("create date"+crDt);
-            System.out.println("status"+status);
+            System.out.println("account number" + acNum);
+            System.out.println("amount" + amt);
+            System.out.println("account holder name" + acHldNm);
+            System.out.println("create date" + crDt);
+            System.out.println("status" + status);
         }
+        connection.commit();
         rs.close();
     }
-    public void DisplayBalance(int accountnumber){
-        System.out.println("Enter account number");
+    public void findByAccountNum(int num) throws SQLException {
+        String sql = "select * from bank_account where acc_num = ? ";
+        PreparedStatement ps = connection.prepareStatement(sql);
+        ps.setInt(1,num);
+
+        ResultSet rs = ps.executeQuery();
+
+        while(rs.next()){
+
+            double amt = rs.getDouble("amt_num");
+
+            System.out.println("amount "+ amt);
+
+        }
+        if(rs.next() == false) System.out.println("Invalid Account Number");
+        connection.commit();
+        rs.close();
+
 
     }
 
-}
 
+
+}
 

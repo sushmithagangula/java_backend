fcf0000445f5429810f3a673cc06fdc05cb4fba8
sushmitha.gangula <sushmitha.gangula@collabera.com>
2022-01-12 17:28:53 +0530

    spring data:basic setup

---
+++ java-simple-app/week2/day3/orm-demo/src/main/java/com/ani/orm/OrmDemoApplication.java
@@ -0,0 +1,14 @@
+package com.ani.orm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrmDemoApplication {
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(OrmDemoApplication.class, args);
+	}
+
+}

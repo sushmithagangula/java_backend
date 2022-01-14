c42c181b9e0689a0d5f31c9b2c4e141e2dff133e
sushmitha.gangula <sushmitha.gangula@collabera.com>
2022-01-14 08:38:49 +0530

    spring data:basic setup

--- java-simple-app/week2/day3/orm-demo/src/main/java/com/ani/orm/repository/CarCurdRepo.java
+++ java-simple-app/week2/day3/orm-demo/src/main/java/com/ani/orm/repository/CarCurdRepo.java
@@ -1,10 +1,14 @@
 package com.ani.orm.repository;
 
-import com.ani.orm.domain.Car;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
+        import com.ani.orm.domain.Car;
+        import org.springframework.data.repository.CrudRepository;
+        import org.springframework.stereotype.Repository;
+
+        import java.util.List;
 
 @Repository
-public interface CarCrudRepo extends CrudRepository<Car, Long > { }
-@override
-final
+public interface CarCrudRepo extends CrudRepository<Car, Long > {
+
+    @Override
+    List<Car> findAll();
+}

2b6e64ea50f6b91331ac6987b5f83d8ef71d31fe
sushmitha.gangula <sushmitha.gangula@collabera.com>
2022-01-15 22:55:33 +0530

    case study architecture

--- java-simple-app/week2/day4/week-bank-rest-app/src/main/java/com/weektwo/casestudy/service/BankService.java
+++ java-simple-app/week2/day4/week-bank-rest-app/src/main/java/com/weektwo/casestudy/service/BankService.java
@@ -1,12 +1,10 @@
 package com.weektwo.casestudy.service;
-
-        import com.weektwo.casestudy.domain.BankAccount;
-
-        import java.util.List;
+import com.weektwo.casestudy.domain.BankAccount;
+import java.util.List;
 
 public interface BankService {
-
     int createNewAccount(BankAccount ba);
+    void createNewAccount(BankAccount ba);
 
     int updateAccountDetails(BankAccount ba);
 
@@ -15,12 +13,17 @@ public interface BankService {
     boolean deActivateAccount(Long acNum);
 
     double withdraw(Long acNum, int amt);
+    double withdraw(Long acNum, double amt);
 
     double deposit(Long acNum, int amt);
+    double deposit(Long acNum, double amt);
 
     int transferMoney(Long srcAc, Long dstAc, int amt);
+    int transferMoney(Long srcAc, Long dstAc, double amt);
 
     BankAccount findAccountByAcNum(Long acNum);
 
     List<BankAccount> findAllBankAccounts();
+
+    List<BankAccount> namesStartsWith(String prefix);
 }
--- java-simple-app/week2/day4/week-bank-rest-app/src/main/java/com/weektwo/casestudy/service/BankServiceImpl.java
+++ java-simple-app/week2/day4/week-bank-rest-app/src/main/java/com/weektwo/casestudy/service/BankServiceImpl.java
@@ -1,60 +1,69 @@
 package com.weektwo.casestudy.service;
-
 import com.weektwo.casestudy.domain.BankAccount;
 import com.weektwo.casestudy.repository.BankRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
 public class BankServiceImpl implements BankService{
-
     @Autowired
     private BankRepository repository;
 
     @Override
     public int createNewAccount(BankAccount ba) {
         return 0;
-    }
+        public void createNewAccount(BankAccount ba) {
+            repository.save(ba);
+        }
 
-    @Override
-    public int updateAccountDetails(BankAccount ba) {
-        return 0;
-    }
+        @Override
+        public int updateAccountDetails(BankAccount ba) {
+            return 0;
+        }
+        @Override
+        public boolean activateAccount(Long acNum) {
+            return false;
+        }
+        @Override
+        public boolean deActivateAccount(Long acNum) {
+            return false;
+        }
 
-    @Override
-    public boolean activateAccount(Long acNum) {
-        return false;
-    }
+        @Transactional
+        @Override
+        public double withdraw(Long acNum, int amt) {
+            return 0;
+            public double withdraw(Long acNum, double amt) {
+                repository.withdraw(amt, acNum);
+                return amt;
+            }
 
-    @Override
-    public boolean deActivateAccount(Long acNum) {
-        return false;
-    }
+            @Override
+            public double deposit(Long acNum, int amt) {
+                public double deposit(Long acNum, double amt) {
+                    return 0;
+                }
 
-    @Override
-    public double withdraw(Long acNum, int amt) {
-        return 0;
-    }
+                @Override
+                public int transferMoney(Long srcAc, Long dstAc, int amt) {
+                    public int transferMoney(Long srcAc, Long dstAc, double amt) {
+                        return 0;
+                    }
 
-    @Override
-    public double deposit(Long acNum, int amt) {
-        return 0;
-    }
+                    @Override
+                    public BankAccount findAccountByAcNum(Long acNum) {
+                        return null;
+                    }
+                    @Override
+                    public List<BankAccount> findAllBankAccounts() {
+                        return null;
+                    }
 
-    @Override
-    public int transferMoney(Long srcAc, Long dstAc, int amt) {
-        return 0;
-    }
-
-    @Override
-    public BankAccount findAccountByAcNum(Long acNum) {
-        return null;
-    }
-
-    @Override
-    public List<BankAccount> findAllBankAccounts() {
-        return null;
-    }
-}
+                    @Override
+                    public List<BankAccount> namesStartsWith(String prefix) {
+                        return repository.findByAcHldNmStartingWith(prefix);
+                    }
+                }

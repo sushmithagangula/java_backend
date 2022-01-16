0650340036d09510420f7436228131b370539fd6
sushmitha.gangula <sushmitha.gangula@collabera.com>
2022-01-16 15:00:55 +0530

    week2_case_study

---
+++ java-simple-app/week2/day5/week2-case-study/src/main/java/com/ani/banking/db/Connectivity.java
@@ -0,0 +1,31 @@
+package com.ani.banking.db;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+//import com.mysql.cj.jdbc.Driver;
+
+public class Connectivity {
+
+    private final Connection connection;
+
+    public Connectivity() throws SQLException {
+
+      //  new Driver(); // initialize the mysql driver
+
+        connection = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/week2_case_study_db",
+                "root",
+                "password"
+        ); // trying to make a connectivity
+
+        connection.setAutoCommit(false); // enabling txn management
+        System.out.println("---- Connected to MySQL Successfully ----");
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+}

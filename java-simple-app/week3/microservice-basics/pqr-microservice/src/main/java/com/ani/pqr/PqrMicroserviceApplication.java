c941e5dbb40fd952a84afe422627b5b6ab41dc66
sushmitha.gangula <sushmitha.gangula@collabera.com>
2022-01-17 14:54:49 +0530

    microservice:second microservice

---
+++ java-simple-app/week3/microservice-basics/pqr-microservice/src/main/java/com/ani/pqr/PqrMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.ani.pqr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PqrMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PqrMicroserviceApplication.class, args);
+	}
+
+}

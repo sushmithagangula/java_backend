021fcf1cc779be871bfeb6f4cb098b80a0a4b8a4
sushmitha.gangula <sushmitha.gangula@collabera.com>
2022-01-18 12:54:28 +0530

    dockerization:created controller and tests

---
+++ java-simple-app/week3/day2/dockerized-app/src/test/java/com/ani/docker/DockerizedAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ani.docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerizedAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
